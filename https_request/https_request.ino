1f96725619d519366d82982d0d15320d765df438
Aaditya <aaditya.s889@gmail.com>
2020-03-24 04:41:36 +0530

    initial commit

--- https_request/https_request.ino
+++ https_request/https_request.ino
@@ -119,7 +119,7 @@ void https_post(String host, String api, const char *fingerprint, String http_pa
 void send_slack_message(String from_username, String destination, String message, String icon)
 {
   const char *host = "hooks.slack.com";
-  String api = "/services/T034MTGTM/B32G6C3EV/UgJsOUiHlRVqnfn5LLtksHZV";
+  String api = "/services/";
   const char *slack_fingerprint = "C1 0D 53 49 D2 3E E5 2B A2 61 D5 9E 6F 99 0D 3D FD 8B B2 B3";
   String http_headers = "{\"Content-Type\": \"application/json\"}";
   
