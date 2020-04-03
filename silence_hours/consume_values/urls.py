ee2f7fbb583945cc5d48581ed6c4a8457dbc84a2
shashank <shashanksah@UCLP1542.local>
2020-04-03 19:35:54 +0530

    change

--- silence_hours/consume_values/urls.py
+++ silence_hours/consume_values/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('get_data', views.get_data, name='get_data'),
+    path('set_data', views.set_data, name='set_data'),
 ]
--- silence_hours/consume_values/views.py
+++ silence_hours/consume_values/views.py
@@ -8,11 +8,20 @@ import mysql.connector as mysql
 db = mysql.connect(
     host = "localhost",
     user = "root",
-    passwd = "asdfg12345"
+    passwd = "asdfg12345",
+    database = "pod_assistant"
 )
 
 cursor = db.cursor()
 #cursor.execute("CREATE DATABASE pod_assistant")
+cursor.execute("""CREATE TABLE IF NOT EXISTS audio (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    pod_name VARCHAR(255), 
+    value INT, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    cerated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
+
+INSERT_IN_AUDIO_QUERY = 'INSERT INTO audio (pod_name, value) VALUES ("{}", {})'
 
 print("db conn: ",db)
 
@@ -24,3 +33,13 @@ def get_data(request):
         "shashank": "sah"
     }
     return JsonResponse(x, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def set_data(request):
+
+    print(request.data)
+
+    query = INSERT_IN_AUDIO_QUERY.format(request.data['pod_name'], request.data['value'])
+    cursor.execute(query)
+    db.commit()
+    return JsonResponse({}, status=status.HTTP_200_OK)
