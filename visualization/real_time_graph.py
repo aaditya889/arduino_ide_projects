ce65e8e451bf9ebdee04989a3dafa8909ab039bd
Karuna Saini <karunasaini@UCLP1543.local>
2020-04-04 16:38:05 +0530

    fixed real-time visualization

--- visualization/real_time_graph.py
+++ visualization/real_time_graph.py
@@ -8,29 +8,27 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 API_URL = "http://localhost:8000/consume/get_latest_data"
 API_PARAMS = {
-  'time_in_secs': 60
+  'time_in_secs': 900000
 }
-
 
 def animate(i):
   curr_time = datetime.now()
   response = requests.post(url=API_URL, data=API_PARAMS)
   pullData = response.json()
-  print(pullData)
   xar = []
   yar = []
   for eachRow in pullData['data']:
-    then_time = datetime.strptime(eachRow[3], '%Y-%m-%dT%H:%M:%S')
-    diff_in_secs = (curr_time - then_time).total_seconds()
-    x = diff_in_secs
-    y = eachRow[2]
-    print("x", diff_in_secs)
-    print("y", eachRow[2])
-    xar.append(int(x))
-    yar.append(int(y))
+    timestamp = eachRow[3]
+    noise_level = eachRow[2]
+    xar.append(timestamp)
+    yar.append(int(noise_level))
   ax1.clear()
   ax1.plot(xar,yar)
-  API_PARAMS['time_in_secs'] = 30
+  plt.xlabel('time', fontsize=10)
+  plt.ylabel('noise level', fontsize=10)
+  plt.xticks(rotation=30)
+  plt.xticks(xar, "")
+  plt.title("NOISE TREND")
 
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
