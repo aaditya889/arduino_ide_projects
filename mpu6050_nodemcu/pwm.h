1e301919a5ac4e054a884b5ef71067d667f0ec73
Aaditya <aaditya.s889@gmail.com>
2020-06-14 01:30:15 +0530

    live test 3.1

--- mpu6050_nodemcu/pwm.h
+++ mpu6050_nodemcu/pwm.h
@@ -11,26 +11,35 @@ Servo ESC_FRONT_2;
 Servo ESC_REAR_1;
 Servo ESC_REAR_2;
 
+//  function definitions
+extern void send_udp(char *);
 
 void calibrate_esc()
 {
+  char udp_message[150];
+  send_udp("Calibrating esc...\n");
   ESC_FRONT_1.attach(FRONT_PIN_1, 1000, 2000);
   ESC_FRONT_2.attach(FRONT_PIN_2, 1000, 2000);
   ESC_REAR_1.attach(REAR_PIN_1, 1000, 2000);
   ESC_REAR_2.attach(REAR_PIN_2, 1000, 2000);
   Serial.println("Sending max pulse...");
+  sprintf(udp_message, "Sending max pulse = %d...", MAX_THRUST);
+  send_udp(udp_message);
   ESC_FRONT_1.write(MAX_THRUST);
   ESC_FRONT_2.write(MAX_THRUST);
   ESC_REAR_1.write(MAX_THRUST);
   ESC_REAR_2.write(MAX_THRUST);
   delay(3000);
   Serial.println("Sending min pulse...");
+  sprintf(udp_message, "Sending min pulse = %d...", MIN_THRUST);
+  send_udp(udp_message);
   ESC_FRONT_1.write(MIN_THRUST);
   ESC_FRONT_2.write(MIN_THRUST);
   ESC_REAR_1.write(MIN_THRUST);
   ESC_REAR_2.write(MIN_THRUST);
-  delay(3000);
+  delay(5000);
   Serial.println("ESC calibrated (check it manually)!");
+  send_udp("ESC calibrated (check it manually)!");
 }
 
 
